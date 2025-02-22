d2b457f51b6caff2e0d22bce2bcb0c34afe5ee43
Nibir2405 <nibirislam56@gmail.com>
2025-02-23 02:05:55 +0600

    Enhance Contact_Us.py to send formatted email messages upon form submission

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,11 +1,19 @@
 import streamlit as st
+from send_email import send_mail
 
 st.header("Contact Me")
 
-with st.form(key="contact from"):
+with st.form(key="contact form"):
 
     user_email = st.text_input("Your Email Address")
-    message = st.text_area("Your Message")
+    raw_message = st.text_area("Your Message")
+    message = f"""\
+    Subject: New mail about portfolio Webapp
+
+    From: {user_email}
+    {raw_message}"""
+
     submit = st.form_submit_button()
     if submit:
-        print("submitted")
+        send_mail(message)
+        st.info("Your message was sent successfully")
