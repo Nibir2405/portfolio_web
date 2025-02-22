67946a7a06b6b3f5beefa0c49bda70e51cbbca83
Nibir2405 <nibirislam56@gmail.com>
2025-02-23 02:23:23 +0600

    Add environment variables for SMTP configuration and update send_email.py to use them

--- send_email.py
+++ send_email.py
@@ -1,18 +1,21 @@
 import smtplib
 import ssl
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
 
-    email = "nibirislam56@gmail.com"
-
-    password = "xxabkwlcvtydocoj"
-
-    receiver = "navidulislam2002@gmail.com"
+    email = os.getenv("SMTP_EMAIL")
+    password = os.getenv("SMTP_PASSWORD")
+    receiver = os.getenv("SMTP_RECEIVER")
 
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(email,password)
+        server.login(email, password)
         server.sendmail(email, receiver, message)
